b15c61eee04212d3095ddb48c15e6fc88e04c59b
Hudhayfah Khan <16khanh@utcsheffield.org.uk>
2022-01-31 14:52:59 +0000

    Add project files.

---
+++ simple card game/Class card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_card_game
+{
+    class CCard
+    {
+        private int suit;
+        private int value;
+
+        public CCard() //empty constructor
+        {
+            suit = 0;
+            value = 0;
+        }
+    }
+    public CCard(int suit, int value) //constructor with parameters
+    {
+        this.suit = suit;
+        this.value = value;
+
+    }    
+         
+         
+
+
+
+
+
+}
